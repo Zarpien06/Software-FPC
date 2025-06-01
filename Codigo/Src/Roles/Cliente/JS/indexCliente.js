5e51deb3aebca11a99cb3d95360447a397fd3418
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-06-01 14:18:22 -0500

    Delete Codigo/Src/Roles directory

--- Codigo/Src/Roles/Cliente/JS/indexCliente.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menuToggle').addEventListener('click', function() {
-    document.getElementById('sidebar').style.width = '250px';
-    document.querySelector('.top-bar').style.marginLeft = '250px';
-});
-
-document.getElementById('closeSidebar').addEventListener('click', function() {
-    document.getElementById('sidebar').style.width = '0';
-    document.querySelector('.top-bar').style.marginLeft = '0';
-});
